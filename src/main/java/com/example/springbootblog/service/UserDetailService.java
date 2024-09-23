6f7cdc6241e6a6883c7aa42528ee1068d4d93b13
SHEEN <normengdie@pusan.ac.kr>
2024-09-23 23:54:42 +0900

    feat : implements User Service method

---
+++ src/main/java/com/example/springbootblog/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package com.example.springbootblog.service;
+
+import com.example.springbootblog.domain.User;
+import com.example.springbootblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
